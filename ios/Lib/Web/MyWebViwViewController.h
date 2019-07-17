e7fd1d2c52481fc0028bde5a29d72ccc7c6fa24f
baisedeheibao <qtz@zhangzedeMacBook-Pro-2.local>
2019-07-17 18:21:48 +0800

    修改web框架

--- ios/Lib/Web/MyWebViwViewController.h
+++ ios/Lib/Web/MyWebViwViewController.h
@@ -1,26 +1,26 @@
 //
-//  MyWebViwViewController.h
-//  chaoshimiao
+//  WLWebController.h
+//  WangliBank
 //
-//  Created by zhangze on 2019/4/14.
-//  Copyright © 2019 gjf. All rights reserved.
+//  Created by 王启镰 on 16/6/21.
+//  Copyright © 2016年 iSoftstone infomation Technology (Group) Co.,Ltd. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-NS_ASSUME_NONNULL_BEGIN
-
 @interface MyWebViwViewController : UIViewController
 
+@property (nonatomic, copy) NSString *url;
+
+  
 @property (nonatomic, copy) NSString *urlStr;
 @property (nonatomic, strong) NSString *titleString;
 @property (nonatomic, strong) NSArray *dataArray;
-
+  
 + (instancetype)sharedSingleton;
-
+  
 - (void)startReload;
-
+  
 - (void)yuLoad;
+  
 @end
-
-NS_ASSUME_NONNULL_END
